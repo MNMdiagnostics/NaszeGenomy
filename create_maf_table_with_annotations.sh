45fda8fb681073bfd83ff0207649b5be38f5bbcb
seru71 <2869419+seru71@users.noreply.github.com>
2021-07-05 12:57:44 +0200

    removed create_maf_table_with_annotations.sh

--- create_maf_table_with_annotations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-VEP_TABLE=multisample_${date}.dv.norm.tsv.gz
-
-bcftools query -f "%INFO/AF\n" /mnt/vault1/mnmorigin_pipeline_out/multisample_${date}.dv.norm.ACANAF.vcf.gz | paste ${VEP_TABLE} - > ${VEP_TABLE}.MAF.tsv
-
