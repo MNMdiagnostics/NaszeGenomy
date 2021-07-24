ae0091a540f18252eb3f12ec9590f69ab80dcc73
seru71 <2869419+seru71@users.noreply.github.com>
2021-07-25 00:25:13 +0200

    added filter annotations to sv

--- vcf_processing/sv_postprocess.sh
+++ vcf_processing/sv_postprocess.sh
@@ -5,9 +5,24 @@
 
 prefix=${1%".vcf.gz"}
 
-bcftools view -S ../SAMPLES_TO_INCLUDE_210519.txt --drop-genotypes -Ou $1 \
-	| bcftools annotate -Ov -x INFO/SNAME \
+bcftools view -S ../SAMPLES_TO_INCLUDE_210716.txt -Ov $1 \
 	| grep -v '^##smoove_count_stats=' \
 	| grep -v '^##SAMPLE=' \
 	| grep -v '^##bcftools_merge'  \
-	| bcftools view -e "INFO/AC<1" -Oz -o ${prefix}.unrelated.nogt.vcf.gz
+	| bcftools annotate -x INFO/SNAME,INFO/smoove_gene -Ou \
+	| bcftools view -e "INFO/AC<1" -Ou \
+        | bcftools +fill-tags -Ou -- -t AF,HWE \
+	| bcftools filter -m+ -s HWE0 -e 'INFO/HWE<0.01' -Ou \
+	| bcftools filter -m+ -s LOW_MSHQ -e 'INFO/MSHQ<3' -Ou \
+	| bcftools view -e "INFO/AC<1" -Oz -o ${prefix}.unrelated.vcf.gz \
+
+tabix -p vcf ${prefix}.unrelated.vcf.gz
+
+bcftools view --drop-genotypes -Oz -o ${prefix}.unrelated.nogt.vcf.gz ${prefix}.unrelated.vcf.gz
+tabix -p vcf ${prefix}.unrelated.nogt.vcf.gz
+
+bcftools view -e "INFO/AC < 2" -Oz -o ${prefix}.unrelated.nogt.ACgt1.vcf.gz ${prefix}.unrelated.nogt.vcf.gz
+tabix -p vcf ${prefix}.unrelated.nogt.ACgt1.vcf.gz
+md5sum ${prefix}.unrelated.nogt.ACgt1.vcf.gz > ${prefix}.unrelated.nogt.ACgt1.vcf.gz.md5
+
+
