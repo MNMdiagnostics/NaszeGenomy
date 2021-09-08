10d81f946ae2b3405e444fe758c05b7c89ba0579
sypniewski20 <mateusz.sypniewski@mnm.bio>
2021-09-08 13:56:22 +0200

    fixed af plot

--- variants_af/af_check.R
+++ variants_af/af_check.R
@@ -2,17 +2,20 @@ suppressMessages(library(tidyverse))
 suppressMessages(library(data.table))
 
 
-af_check <- fread('variants_af/af_check.tsv',header=T)
+af_check <- fread('af_check.tsv',header=T) %>% 
+  mutate(gnomADg_AF_NFE = as.numeric(gnomADg_AF_NFE)) %>%
+  mutate(gnomADg_AF_NFE = ifelse(is.na(gnomADg_AF_NFE)==T, 1e-5, gnomADg_AF_NFE))
 
-#af_check <- af_check %>% filter(gnomADg_AF_NFE == '-')
+
 
 af_check <- af_check %>% select(Uploaded_variation,PL_AF,gnomADg_AF_NFE) %>% 
-  mutate(gnomADg_AF_NFE = ifelse(gnomADg_AF_NFE == '-', 0,af_check$gnomADg_AF_NFE)) %>%
-  mutate(gnomADg_AF_NFE = as.numeric(af_check$gnomADg_AF_NFE)) %>%
   filter(PL_AF < 0.01)
+  
 sum(is.na(af_check$gnomADg_AF_NFE))
 
-af_check %>% ggplot(aes(x=PL_AF,y=log10(gnomADg_AF_NFE))) + 
-  geom_bin2d(bins = 70) +
-  scale_fill_continuous(type = "viridis")  +
-  theme_minimal()
+af_check %>% ggplot(aes(x=PL_AF,y=gnomADg_AF_NFE)) + 
+  #geom_bin2d(bins = 70) +
+  #scale_fill_continuous(type = "viridis")  +
+  stat_binhex()
+  geom_smooth(formula= y ~ x, method='lm') +
+  ggsave('af_check.png')
